40fa4f988cb6cf93c60878f536e76fe9e52daa5b
Shtrubin <rubeensht17@gmail.com>
2023-10-15 07:52:20 +0545

    Created a animated gradient Baclgroud, logo desiged, navbar and landing screen. Along with Brands Section

---
+++ js/index.js
@@ -0,0 +1,58 @@
+//    toggle dropdown arrow while hover
+const b = document.querySelector("#drop-1");
+const a = document.querySelector("#drop1");
+
+
+b.addEventListener("mouseenter", () => {
+  if (a.classList.contains("fa-chevron-down")) {
+    // If the "fa-chevron-down" class is present on hover, remove it and add "fa-angle-up"
+    a.classList.remove("fa-chevron-down");
+    a.classList.add("fa-angle-up");
+  } else if (a.classList.contains("fa-angle-up")) {
+    // If the "fa-angle-up" class is present on hover, remove it and add "fa-chevron-down"
+    a.classList.remove("fa-angle-up");
+    a.classList.add("fa-chevron-down");
+  }
+});
+
+a.addEventListener("mouseleave", () => {
+  if (a.classList.contains("fa-chevron-down")) {
+    // If the "fa-chevron-down" class is present on mouseleave, remove it and add "fa-angle-up"
+    a.classList.remove("fa-chevron-down");
+    a.classList.add("fa-angle-up");
+  } else if (a.classList.contains("fa-angle-up")) {
+    // If the "fa-angle-up" class is present on mouseleave, remove it and add "fa-chevron-down"
+    a.classList.remove("fa-angle-up");
+    a.classList.add("fa-chevron-down");
+  }
+});
+
+
+   
+const c = document.querySelector("#drop-2");
+const d = document.querySelector("#drop2");
+
+
+c.addEventListener("mouseenter", () => {
+  if (d.classList.contains("fa-chevron-down")) {
+    // If the "fa-chevron-down" class is present on hover, remove it and add "fa-angle-up"
+    d.classList.remove("fa-chevron-down");
+    d.classList.add("fa-angle-up");
+  } else if (d.classList.contains("fa-angle-up")) {
+    // If the "fa-angle-up" class is present on hover, remove it and add "fa-chevron-down"
+    d.classList.remove("fa-angle-up");
+    d.classList.add("fa-chevron-down");
+  }
+});
+
+d.addEventListener("mouseleave", () => {
+  if (d.classList.contains("fa-chevron-down")) {
+    // If the "fa-chevron-down" class is present on mouseleave, remove it and add "fa-angle-up"
+    d.classList.remove("fa-chevron-down");
+    d.classList.add("fa-angle-up");
+  } else if (d.classList.contains("fa-angle-up")) {
+    // If the "fa-angle-up" class is present on mouseleave, remove it and add "fa-chevron-down"
+    d.classList.remove("fa-angle-up");
+    d.classList.add("fa-chevron-down");
+  }
+});
